f7705adaebdc0306648c1339dfe06b52ebe6e272
Suleman Mac <user@USERs-MacBook-Pro.local>
2023-11-01 18:28:46 +0500

    update script:activeRunner

--- activeRunner.sh
+++ activeRunner.sh
@@ -2,9 +2,8 @@
 
 # Function to simulate right and down arrow key presses
 simulate_keyboard_click() {
-    echo "in func"
     osascript -e 'tell application "System Events" to key code 49' # Right arrow key
-    osascript -e 'tell application "System Events" to key code 127' # Right arrow key
+    osascript -e 'tell application "System Events" to key code 51' # Backspace key to delete the space
 }
 
 # Display a message every time the script runs
@@ -12,5 +11,7 @@ echo "Keyboard click simulation script ran at $(date)"
 
 # Simulate right and down arrow key presses
 simulate_keyboard_click
+
+#/Users/suleman/Suleman/Hashmaker/muyasr-app/frontend/src/utils/common
 
 # * * * * * /Users/suleman/Suleman/bashScript/activeRunner.sh >> /Users/suleman/Suleman/bashScript/activeRunner.log 2>&1
